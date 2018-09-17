eff496c9db67760e114677be17c6ddaf1609f22f
zwb <308539393@qq.com>
2018-09-17 09:12:41 +0800

    二维数组输出

--- netty/src/test/java/com/zhaowb/netty/Test.java
+++ netty/src/test/java/com/zhaowb/netty/Test.java
@@ -2,25 +2,12 @@ package com.zhaowb.netty;
 
 public class Test {
     public static void main(String[] args) {
-
-        StringBuilder sb = new StringBuilder();
-        String s = "B1201\n" +
-                "B1202\n" +
-                "B1203\n" +
-                "B1206\n" +
-                "B1207\n" +
-                "B1208\n" +
-                "B1301\n" +
-                "B1302\n" +
-                "B1303\n" +
-                "B1306\n" +
-                "B1307\n";
-        String[] ss = s.split("\n");
-        for (String m : ss) {
-            sb.append(m + ",");
-        }
-        sb.deleteCharAt(sb.length() - 1);
-
-        System.out.println(sb.toString());
+        int[][] nums = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+      for (int i = 0 ;i < nums.length;i++){
+          for (int j = 0;j<nums[i].length;j++){
+              System.out.print(nums[i][j] + " ");
+          }
+          System.out.println();
+      }
     }
 }
