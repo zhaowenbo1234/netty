45ff77c1541cc53651fbd7eb870b82bbf9837e32
zwb <308539393@qq.com>
2018-12-10 17:18:20 +0800

    使用java.net的URL类获取URL的各个部分参数：

---
+++ netty/src/main/java/com/zhaowb/netty/javabase/url/URLDemo.java
@@ -0,0 +1,30 @@
+package com.zhaowb.netty.javabase.url;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created with IDEA
+ * 使用java.net的URL类获取URL的各个部分参数：
+ * @author zwb
+ * @create 2018/12/10 17:16
+ */
+public class URLDemo {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://www.runoob.com/index.html?language=cn#j2se");
+            System.out.println("URL 为：" + url.toString());
+            System.out.println("协议为：" + url.getProtocol());
+            System.out.println("验证信息：" + url.getAuthority());
+            System.out.println("文件名及请求参数：" + url.getFile());
+            System.out.println("主机名：" + url.getHost());
+            System.out.println("路径：" + url.getPath());
+            System.out.println("端口：" + url.getPort());
+            System.out.println("默认端口：" + url.getDefaultPort());
+            System.out.println("请求参数：" + url.getQuery());
+            System.out.println("定位位置：" + url.getRef());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
